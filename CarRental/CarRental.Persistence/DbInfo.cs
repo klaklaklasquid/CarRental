739abbe161df0064e7501c9c152678c84250eccf
klaklaklasquid <126030891+klaklaklasquid@users.noreply.github.com>
2025-05-10 12:04:20 +0200

    added the database

--- CarRental/CarRental.Persistence/DbInfo.cs
+++ CarRental/CarRental.Persistence/DbInfo.cs
@@ -1,5 +1,5 @@
 ﻿namespace CarRental.Persistence {
     internal static class DbInfo {
-        internal const string ConnectionString = @"";
+        internal const string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Gebruiker\Desktop\CarRental\CarRental\CarRentalDatabase.mdf;Integrated Security=True;Connect Timeout=30";
     }
 }
